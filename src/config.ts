3a33e8f4bcd789cc86a88bc64f7d4be575cac2ce
Mahyar Mirrashed <59240843+mahyarmirrashed@users.noreply.github.com>
2021-06-25 07:29:47 -0500

    refactor: improve configuration names, docs and coding styles (#15)
    * Whitespace formatting with TABs instead of spaces
    
    * TSLint is now deprecated (use eslint-typescript instead)
    
    * Reformat for easier debugging later on
    
    * Update property names and descriptions
    
    * Upgrade to at least version 1.50.0 of VSCode engine
    
    * Rename property names
    
    * Reorder properties
    
    * Formalize README contents
    
    * Simply write MIT instead
    
    * Cleaned up strings and removed uncessary argument
    
    * Changed to new property names
    
    * Cleaned up strings
    
    * Updated CHANGELOG.md

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,27 @@
-import * as vscode from 'vscode';
-import { IVsCodeConfig } from './lib/interface';
+import * as vscode from "vscode";
+import { IVsCodeConfig } from "./lib/interface";
 
 export function getConfig(): IVsCodeConfig {
-  let config: IVsCodeConfig = {
-    ignore: [
-      'node_modules'
-    ],
-    rootCharCode: vscode.workspace.getConfiguration().get<number>("Root element character code"),
-    childCharCode: vscode.workspace.getConfiguration().get<number>("Child element character code"),
-    lastCharCode: vscode.workspace.getConfiguration().get<number>("Last element character code"),
-    parentCharCode: vscode.workspace.getConfiguration().get<number>("Parent element character code"),
-    dashCharCode: vscode.workspace.getConfiguration().get<number>("Dash element character code"),
-    blankCharCode: vscode.workspace.getConfiguration().get<number>("Blank element character code")
-  };
-  return config;
+    let config: IVsCodeConfig = {
+        ignore: ["node_modules"],
+        rootCharCode: vscode.workspace
+            .getConfiguration()
+            .get<number>("asciiTreeGenerator.rootElement"),
+        parentCharCode: vscode.workspace
+            .getConfiguration()
+            .get<number>("asciiTreeGenerator.parentElement"),
+        childCharCode: vscode.workspace
+            .getConfiguration()
+            .get<number>("asciiTreeGenerator.childElement"),
+        lastCharCode: vscode.workspace
+            .getConfiguration()
+            .get<number>("asciiTreeGenerator.lastElement"),
+        dashCharCode: vscode.workspace
+            .getConfiguration()
+            .get<number>("asciiTreeGenerator.dashElement"),
+        blankCharCode: vscode.workspace
+            .getConfiguration()
+            .get<number>("asciiTreeGenerator.blankElement"),
+    };
+    return config;
 }
-
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,36 +1,48 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as assert from 'assert';
-import { revertTreeString } from '../utils';
+import * as fs from "fs";
+import * as path from "path";
+import * as assert from "assert";
+import { revertTreeString } from "../utils";
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite('Extension Tests', function () {
-  this.timeout(120000);
+suite("Extension Tests", function () {
+    this.timeout(120000);
 
-  // Defines a Mocha unit test
-  // test("Something 1", function() {
-  //     assert.equal(-1, [1, 2, 3].indexOf(5));
-  //     assert.equal(-1, [1, 2, 3].indexOf(0));
-  // });
-  test('revert tree string to text', function () {
-    const rootText = fs.readFileSync(path.join(__dirname, '../../fixtures/root.txt'), 'utf8');
-    const rootReverted = fs.readFileSync(path.join(__dirname, '../../fixtures/root-reverted.txt'), 'utf8');
-    const reverted = revertTreeString(rootText);
-    const a = reverted.trim().split('\n');
-    const b = rootReverted.trim().split('\n');
-    a.forEach((line, idx) => {
-      if (a[idx] !== b[idx]) {
-        console.log(a[idx], b[idx]);
-      }
+    // Defines a Mocha unit test
+    // test("Something 1", function() {
+    //     assert.equal(-1, [1, 2, 3].indexOf(5));
+    //     assert.equal(-1, [1, 2, 3].indexOf(0));
+    // });
+    test("revert tree string to text", function () {
+        const rootText = fs.readFileSync(
+            path.join(__dirname, "../../fixtures/root.txt"),
+            "utf8"
+        );
+        const rootReverted = fs.readFileSync(
+            path.join(__dirname, "../../fixtures/root-reverted.txt"),
+            "utf8"
+        );
+        const reverted = revertTreeString(rootText);
+        const a = reverted.trim().split("\n");
+        const b = rootReverted.trim().split("\n");
+        a.forEach((line, idx) => {
+            if (a[idx] !== b[idx]) {
+                console.log(a[idx], b[idx]);
+            }
+        });
+        assert(reverted.trim() === rootReverted.trim());
     });
-    assert(reverted.trim() === rootReverted.trim());
-  });
 
-  // https://github.com/aprilandjan/ascii-tree-generator/pull/11
-  test('revert more-depth tree string to text', function () {
-    const origin = fs.readFileSync(path.join(__dirname, '../../fixtures/more-depth-reverted.txt'), 'utf8');
-    const treeString = fs.readFileSync(path.join(__dirname, '../../fixtures/more-depth.txt'), 'utf8');
-    const reverted = revertTreeString(treeString);
-    assert(origin.trim() === reverted.trim());
-  });
+    // https://github.com/aprilandjan/ascii-tree-generator/pull/11
+    test("revert more-depth tree string to text", function () {
+        const origin = fs.readFileSync(
+            path.join(__dirname, "../../fixtures/more-depth-reverted.txt"),
+            "utf8"
+        );
+        const treeString = fs.readFileSync(
+            path.join(__dirname, "../../fixtures/more-depth.txt"),
+            "utf8"
+        );
+        const reverted = revertTreeString(treeString);
+        assert(origin.trim() === reverted.trim());
+    });
 });
